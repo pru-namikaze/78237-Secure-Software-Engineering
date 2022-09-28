6a7c1e940d1f01071f777925d33178c38b8df0bd
Pranshu <pranshurajgoel@gmail.com>
2022-09-28 19:22:55 +0930

    Fixed the pom and application.properties and minorr factoring defect

---
+++ src/main/java/com/universityofadelaide/sseproject/SseprojectApplication.java
@@ -0,0 +1,13 @@
+package com.universityofadelaide.sseproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseprojectApplication.class, args);
+    }
+
+}
