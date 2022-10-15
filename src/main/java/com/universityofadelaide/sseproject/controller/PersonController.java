6514d69c78205dad642dc2a7c85237b2299a99a2
Pranshu <pranshurajgoel@gmail.com>
2022-10-15 13:37:17 +1030

    Added Thymeleaf and login/ Register Page

--- src/main/java/com/universityofadelaide/sseproject/controller/PersonController.java
+++ src/main/java/com/universityofadelaide/sseproject/controller/PersonController.java
@@ -3,9 +3,16 @@ package com.universityofadelaide.sseproject.controller;
 import com.universityofadelaide.sseproject.controller.generic.GenericController;
 import com.universityofadelaide.sseproject.model.Person;
 import com.universityofadelaide.sseproject.model.repository.PersonRepository;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/person")
+@Controller
 public class PersonController extends GenericController<Person, PersonRepository> {
+  @RequestMapping("/profile")
+  public ModelAndView getProfile(){
+    return new ModelAndView("profile.html");
+  }
 }
